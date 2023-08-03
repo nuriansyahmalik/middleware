c17a74231884b48b2d67c0e8843c891e7d461d48
Muhammad Nuriansyah Malik Nugraha <nuriansyah.malik@evermos.com>
2023-08-03 10:00:36 +0700

    add: "Function Generate JWT & Validation JWT"

--- main.go
+++ main.go
@@ -11,6 +11,19 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+	router(r)
+
+	jwtTOkens, _ := generateJwt()
+	fmt.Println(jwtTOkens)
+	fmt.Println(validateJwt(jwtTOkens))
+
+	http.ListenAndServe(":8080", r)
+}
+
+var xApiKey = "apikey"
+var secretKey = []byte("secret")
+
+func router(r chi.Router) http.Handler {
 	r.Use(middleware.Logger)
 	r.Use(middleware.RequestID)
 	r.Use(LoggingMiddleware)
@@ -30,16 +43,8 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Custom Middleware"))
 	})
-
-	jwtTOkens, _ := generateJwt()
-	fmt.Println(jwtTOkens)
-	fmt.Println(validateJwt(jwtTOkens))
-
-	http.ListenAndServe(":8080", r)
+	return r
 }
-
-var xApiKey = "apikey"
-var secret = "secret"
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -69,7 +74,7 @@ func generateJwt() (string, error) {
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-	tokenString, err := token.SignedString([]byte(secret))
+	tokenString, err := token.SignedString(secretKey)
 	if err != nil {
 		return "", err
 	}
@@ -78,12 +83,12 @@ func generateJwt() (string, error) {
 
 func validateJwt(tokenString string) (*Claims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(secret), nil
+		return secretKey, nil
 	})
 	if err != nil {
 		return nil, fmt.Errorf("JWT validation failed, %s", err)
 	}
-	if claim, ok := token.Claims.(*Claims); ok != token.Valid {
+	if claim, ok := token.Claims.(*Claims); ok && token.Valid {
 		return claim, nil
 	}
 	return nil, fmt.Errorf("JWT not valid")
