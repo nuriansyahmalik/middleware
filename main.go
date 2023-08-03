fe4faa1fd8f0b01b799199786d905cb5fa1aaa98
Muhammad Nuriansyah Malik Nugraha <nuriansyah.malik@evermos.com>
2023-08-03 09:22:30 +0700

    add: "Function Generate JWT & Validation JWT"

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/golang-jwt/jwt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -25,14 +27,19 @@ func main() {
 			})
 		})
 	})
-
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Custom Middleware"))
 	})
+
+	jwtTOkens, _ := generateJwt()
+	fmt.Println(jwtTOkens)
+	fmt.Println(validateJwt(jwtTOkens))
+
 	http.ListenAndServe(":8080", r)
 }
 
 var xApiKey = "apikey"
+var secret = "secret"
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -44,4 +51,40 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("X-API-KEY", xApiKey)
 		next.ServeHTTP(w, r)
 	})
+}
+
+type Claims struct {
+	UserID   int    `json:"userID"`
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func generateJwt() (string, error) {
+	claim := Claims{
+		UserID:   123,
+		Username: "dadang ben",
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
+			Issuer:    "evermos",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func validateJwt(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("JWT validation failed, %s", err)
+	}
+	if claim, ok := token.Claims.(*Claims); ok != token.Valid {
+		return claim, nil
+	}
+	return nil, fmt.Errorf("JWT not valid")
 }
